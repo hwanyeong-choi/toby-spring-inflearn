1b33e368963a1079eb96965ea9b2f450ec374e38
THINKPAD-HOANY\hoany <hoany1215@gmail.com>
2024-05-29 20:04:57 +0900

    섹션 3-7 Hello Controller 매핑과 바인딩

--- src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
@@ -35,7 +35,8 @@ public class TobyspringbootApplication {
 		 */
 
 		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-
+			// /hello 경로에 대한 요청을 처리할 수 있는 객체를 구현
+			HelloController helloController = new HelloController();
 			/*
 				serverFactory를 통해서 서블릿 컨테이너가 생성되었다면 서블릿 컨테이너에
 				서블릿을 등록한다. 서블릿을 등록하는건 webserver생성시 ServletContextInitializer을
@@ -50,22 +51,24 @@ public class TobyspringbootApplication {
 						// 현재 접근하는 경로가 /hello인지 확인한다.
 						// 현재 요청 메소드가 GET인지 확인한다.
 						if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+
 							// 파라미터를 전달받는데 name으로 선언된 파라미터를 추출한다.
 							String name = req.getParameter("name");
+
+							// 요청을 처리할 객체에로 파라미터를 전달한다.
+							String ret = helloController.hello(name);
 
 							// 응답코드 설정
 							resp.setStatus(HttpStatus.OK.value());
 							// 헤더에 Content Type 명시
 							resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
 							// Content Type과 일치하는 응답값 바디를 입력
-							resp.getWriter().println("Hello " + name);
+							resp.getWriter().println(ret);
 						} else if(req.getRequestURI().equals("/user")) {
 							//
 						} else {
 							resp.setStatus(HttpStatus.NOT_FOUND.value());
 						}
-						
-
 
 					}
 					// / 모든 요청에대해 처리하기 위해 모든 경로에 해당하는 정규표현식 경로 설정
