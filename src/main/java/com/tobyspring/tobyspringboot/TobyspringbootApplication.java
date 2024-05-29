22a4c4ba1acd5c997ef19fec2f98231c1edaabf7
THINKPAD-HOANY\hoany <hoany1215@gmail.com>
2024-05-29 18:53:18 +0900

    섹션 3-3 서블릿 등록

--- src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
@@ -1,7 +1,20 @@
 package com.tobyspring.tobyspringboot;
 
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import jdk.jfr.ContentType;
 
 public class TobyspringbootApplication {
 
@@ -13,12 +26,35 @@ public class TobyspringbootApplication {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
 		/*
-			웹서버 서블릿 컨테이너를 생성하는 함수
+			웹서버 서블릿 컨테이너를 생성하는 함수 serverFactory.getWebServer()
 			리턴타입이 디폴트로 설정한 Tomcat이라는 명칭은 사라지고  WebServer명칭으로 된이유는
 			스프링 부트가 톰캣 외에 제티나 언더토우같은 다양한 서블릿 컨테이너를 지원할 수 있고
 			지원하되 일관된 방식으로 사용할 수 있도록 동작하게할 수 있도록 추상화 해놨기 때문이다.
 		 */
-		WebServer webServer = serverFactory.getWebServer();
+
+		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+
+			/*
+				serverFactory를 통해서 서블릿 컨테이너가 생성되었다면 서블릿 컨테이너에
+				서블릿을 등록한다. 서블릿을 등록하는건 webserver생성시 ServletContextInitializer을
+				구현하는 객체를 매개변수로 전달하면 된다.
+			 */
+			@Override
+			public void onStartup(ServletContext servletContext) throws ServletException {
+				servletContext.addServlet("hello", new HttpServlet() {
+					@Override
+					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+						// 응답코드 설정
+						resp.setStatus(HttpStatus.OK.value());
+						// 헤더에 Content Type 명시
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain");
+						// Content Type과 일치하는 응답값 바디를 입력
+						resp.getWriter().println("Hello Servlet");
+					}
+					// /hello path 매핑
+				}).addMapping("/hello");
+			}
+		});
 
 		/*
 			Servlet 컨테이너 동작 함수
