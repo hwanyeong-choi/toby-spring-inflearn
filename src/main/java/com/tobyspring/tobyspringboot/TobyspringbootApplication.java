ccd4565a3b1fb345ac396205ec39d186c5045001
THINKPAD-HOANY\hoany <hoany1215@gmail.com>
2024-05-31 15:34:41 +0900

    섹션 4-7 스프링 컨테이너로 통합 이전 커밋은 섹션 4-6 애노테이션 매핑 정보 사용

--- src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
@@ -17,19 +17,15 @@ public class TobyspringbootApplication {
 			Spring Container 구현, DispatcherServlet을 사용하기 위해서는
 			GenericApplicationContext이 아닌 GenericWebApplicationContext 형식으로 생성해야합니다.
 		 */
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-		// Spring Container HelloController Bean등록 클래스의 구성정보 메타정보를 넘겨준다.
-		applicationContext.registerBean(HelloController.class);
-		// Spring Container SimpleHelloService를 Bean등록 클래스의 구성정보 메타정보를 넘겨준다.
-		applicationContext.registerBean(SimpleHelloService.class);
-		// ApplicationContext가 refresh메소드를 통해 빈 오브젝트를 생성합니다.
-		applicationContext.refresh();
-
-		/*
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+				/*
 			Spring Boot에서 Tomcat Sublet 컨테이너를 내장해서 프로그앰에서 코드로
 			쉽게 사용할 수 있도록 제공하는 클래스 TomcatServletWebServerFactory
 		 */
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
 		/*
 			웹서버 서블릿 컨테이너를 생성하는 함수 serverFactory.getWebServer()
@@ -38,26 +34,28 @@ public class TobyspringbootApplication {
 			지원하되 일관된 방식으로 사용할 수 있도록 동작하게할 수 있도록 추상화 해놨기 때문이다.
 		 */
 
-		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-			/*
-				serverFactory를 통해서 서블릿 컨테이너가 생성되었다면 서블릿 컨테이너에
-				서블릿을 등록한다. 서블릿을 등록하는건 webserver생성시 ServletContextInitializer을
-				구현하는 객체를 매개변수로 전달하면 된다.
-			 */
-			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-				//  서블릿 등록
-				servletContext.addServlet("dispatcherServlet",
-					// DispatcherServlet 등록
-					new DispatcherServlet(applicationContext)
-					).addMapping("/*");
-			}
-		});
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					// DispatcherServlet등록
+					servletContext.addServlet("dispatcherServlet",
+						new DispatcherServlet(this))
+						.addMapping("/*");
+				});
 
 		/*
 			Servlet 컨테이너 동작 함수
 		 */
-		webServer.start();
+				webServer.start();
+
+			}
+		};
+		// Spring Container HelloController Bean등록 클래스의 구성정보 메타정보를 넘겨준다.
+		applicationContext.registerBean(HelloController.class);
+		// Spring Container SimpleHelloService를 Bean등록 클래스의 구성정보 메타정보를 넘겨준다.
+		applicationContext.registerBean(SimpleHelloService.class);
+		// ApplicationContext가 refresh메소드를 통해 빈 오브젝트를 생성합니다.
+		applicationContext.refresh();
+
+
 	}
 
 }
