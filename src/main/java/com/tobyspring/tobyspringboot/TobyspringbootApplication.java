756cd2f1b7d1ddcb201e7c1d618eee8bf9f82669
THINKPAD-HOANY\hoany <hoany1215@gmail.com>
2024-05-31 15:09:08 +0900

    reword 섹션 4-7 스프링 컨테이너로 통합

--- src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/tobyspring/tobyspringboot/TobyspringbootApplication.java
@@ -1,21 +1,11 @@
 package com.tobyspring.tobyspringboot;
 
-import java.io.IOException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
