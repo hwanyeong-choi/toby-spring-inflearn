90208eeaf51a69b3ef631f7a98e207ee06e27b09
DESKTOP-K4E22JK\THINKPAD-WINDOWS <hoany1215@gmail.com>
2024-06-13 02:30:15 +0900

    섹션 6-7 자동 구성 애노테이션 적용

--- src/main/java/com/tobyspring/config/MyAutoConfiguration.java
+++ src/main/java/com/tobyspring/config/MyAutoConfiguration.java
@@ -7,7 +7,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/*
+    @Configuration은 독특한 특징이 있습니다.
+ */
+
+/*
+    @Congifuration proxybeanMethods의 값을 flase로 변경하다록 합니다.
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public @interface MyAutoConfiguration { }
