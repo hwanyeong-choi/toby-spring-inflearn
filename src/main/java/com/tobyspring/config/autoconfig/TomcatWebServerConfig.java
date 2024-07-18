4b3735ff58f8487b6ccb2382820b06be44b4d410
DESKTOP-K4E22JK\THINKPAD-WINDOWS <hoany1215@gmail.com>
2024-07-18 20:05:42 +0900

    섹션 8-3 @Value와 PropertySourcesPlaceholderConfigurer

--- src/main/java/com/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.tobyspring.config.autoconfig;
 
 import com.tobyspring.config.ConditionalMyOnClass;
 import com.tobyspring.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -12,6 +13,9 @@ import org.springframework.core.env.Environment;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
 
     /*
@@ -21,9 +25,9 @@ public class TomcatWebServerConfig {
         입니다.
     */
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment environment) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(environment.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }
